f7b57ce8de5050dc835a8a01f0f551d645b80670
Alexander Tse <tsetsealexander@gmail.com>
2019-11-18 14:57:05 +0800

    Add metro user

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -10,3 +10,4 @@
 
 
 hpl_admin:$1$dlw7ZBow$CYsg2ZVSPYTYk/sKAx4EV.:HPL Admin:alexander.tse@horizons-partners.com:admin,user
+metro_user:$1$nzIq8dWN$pqdZvHhLVSvdVM.9vHPdd.:Metro User:metro_user@hnlmetro.com:user
